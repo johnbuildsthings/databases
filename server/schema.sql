92217a59a61822a9a1d44cea386746b77fb4e50d
Hack Reactor Students <accounts@hackreactor.com>
2015-07-24 14:55:31 -0700

    Update our initial database schema with messages and users table.

--- server/schema.sql
+++ server/schema.sql
@@ -4,12 +4,17 @@ USE chat;
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+  userId INT(4),
+  message VARCHAR(160),
+  room VARCHAR(20),
+  time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 /* Create other tables and define schemas for them here! */
-
-
-
+CREATE TABLE users (
+  id INT(4),
+  name VARCHAR(20)
+);
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
