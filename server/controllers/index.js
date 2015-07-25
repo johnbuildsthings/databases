a19cd7accb83bc23104d3e3a1135ac79c142beb9
Hack Reactor Students <accounts@hackreactor.com>
2015-07-25 10:12:09 -0700

    Refactor our controller and move DB specific methods in the DB model..

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,73 +1,23 @@
 var models = require('../models');
 var bluebird = require('bluebird');
 var connection = require('../db/index');
-var jsescape = require('js-string-escape');
-var mysql = require('mysql');
 
-
-
-var addToDB = function(table, rowsArray, valuesArray, cb){
-  connection.dbConnection.getConnection(function(err, connection){
-    if (err){
-      connection.release();
-      console.log('connection error in addToDB');
-      return ;
-    }
-
-    var sql = "INSERT INTO ?? (??) VALUES (?);"
-    var inserts = [table, rowsArray, valuesArray];
-    sql = mysql.format(sql, inserts);
-    connection.query(sql, function(err, result){
-      connection.release();
-      if(err){
-        console.log('error updating users db', err);
-      }else{
-        console.log('success, ', result);
-      }
-    });
-    cb();
-  });
-}
-
-var getFromDB = function(table, returnColumn, searchColumn, value, cb){
-  connection.dbConnection.getConnection(function(err, connection){
-    if(err){
-      connection.release();
-      console.log('connection error in getFromDB');
-      return;
-    }
-    var sql = "SELECT * FROM messages;" // WHERE ?? = (?);"
-    var inserts = [returnColumn, table, searchColumn, value];
-    // sql = mysql.format(sql, inserts);
-    connection.query(sql, function(err, result){
-      connection.release();
-      if(err){
-        console.log('error updating users db', err);
-      }else{
-        console.log('success, ', result);
-        cb(JSON.stringify(result));
-        return result;
-      }
-    });
-  });
-}
 
 
 module.exports = {
   messages: {
     get: function (req, res) {
       var data = req.body;
-      console.log(data);
       if(data === undefined){
 
       }
-      getFromDB('messages', ['message', 'room'], 'message', '*', function(arg){res.end(arg);});
+      connection.getFromDB('messages', ['message', 'room'], 'message', '*', function(arg){res.end(arg);});
     }, // a function which handles a get request for all messages
     post: function (req, res) {
-      console.log('POST MESSAGE');
+      // console.log('POST MESSAGE');
       var data = req.body;
-      console.log(data);
-      addToDB('messages', ['message', 'room'], [data.message, data.roomname], function(){res.end();});
+      // console.log(data);
+      connection.addToDB('messages', ['message', 'room'], [data.message, data.roomname], function(){res.end();});
       // res.end();
 
     } // a function which handles posting a message to the database
@@ -80,9 +30,9 @@ module.exports = {
     },
     post: function (req, res) {
       var data = req.body;
-      console.log(data);
-      console.log(data.username);
-      addToDB('users', ['name'], [data.username],function(){res.end();});
+      // console.log(data);
+      // console.log(data.username);
+      connection.addToDB('users', ['name'], [data.username],function(){res.end();});
       // res.end();
     }
   }
--- server/db/index.js
+++ server/db/index.js
@@ -18,3 +18,49 @@ exports.dbConnection = mysql.createPool({
 //  Empty the db table before each test so that multiple tests
 //  * (or repeated runs of the tests) won't screw each other up: 
 // dbConnection.query("truncate " + tablename, done);
+
+exports.addToDB = function(table, rowsArray, valuesArray, cb){
+  exports.dbConnection.getConnection(function(err, connection){
+    if (err){
+      connection.release();
+      console.log('connection error in addToDB');
+      return ;
+    }
+
+    var sql = "INSERT INTO ?? (??) VALUES (?);"
+    var inserts = [table, rowsArray, valuesArray];
+    sql = mysql.format(sql, inserts);
+    connection.query(sql, function(err, result){
+      connection.release();
+      if(err){
+        console.log('error updating users db', err);
+      }else{
+        console.log('success, ', result);
+      }
+    });
+    cb();
+  });
+}
+
+exports.getFromDB = function(table, returnColumn, searchColumn, value, cb){
+  exports.dbConnection.getConnection(function(err, connection){
+    if(err){
+      connection.release();
+      console.log('connection error in getFromDB');
+      return;
+    }
+    var sql = "SELECT * FROM messages;" // WHERE ?? = (?);"
+    var inserts = [returnColumn, table, searchColumn, value];
+    // sql = mysql.format(sql, inserts);
+    connection.query(sql, function(err, result){
+      connection.release();
+      if(err){
+        console.log('error updating users db', err);
+      }else{
+        console.log('success, ', result);
+        cb(JSON.stringify(result));
+        return result;
+      }
+    });
+  });
+}
